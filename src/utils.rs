0d2a1868d2a6047c9a9b602ac0e92ebf3005f128
Lucop1911 <lucop1911@gmail.com>
2025-06-06 14:40:25 +0200

    aa

--- src/utils.rs
+++ src/utils.rs
@@ -54,7 +54,7 @@ pub fn generate_salt() -> String {
 }
 
 pub fn hash_password(password: &str, salt: &str) -> String {
-    let mut hasher = Sha256::new();
+    let mut hasher = Sha256::default();
     hasher.update(password.as_bytes());
     hasher.update(salt.as_bytes());
     let result = hasher.finalize();
